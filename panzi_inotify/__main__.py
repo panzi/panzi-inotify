13fab3d028da3601fde919cde9b42e1cac4e9701
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2026-01-18 23:04:38 +0100

    --mask option for __main__

--- panzi_inotify/__main__.py
+++ panzi_inotify/__main__.py
@@ -1,16 +1,82 @@
+import re
 import sys
+import argparse
 
-from . import Inotify, get_inotify_event_names, logging, _logger
+import panzi_inotify
+from . import Inotify, get_inotify_event_names, IN_ALL_EVENTS
 
-def main():
-    _logger.setLevel(logging.DEBUG)
+flags = [
+    'IN_ACCESS',
+    'IN_MODIFY',
+    'IN_ATTRIB',
+    'IN_CLOSE_WRITE',
+    'IN_CLOSE_NOWRITE',
+    'IN_OPEN',
+    'IN_MOVED_FROM',
+    'IN_MOVED_TO',
+    'IN_CREATE',
+    'IN_DELETE',
+    'IN_DELETE_SELF',
+    'IN_MOVE_SELF',
+    'IN_ONLYDIR',
+    'IN_DONT_FOLLOW',
+    'IN_EXCL_UNLINK',
+    'IN_MASK_CREATE',
+    'IN_MASK_ADD',
+    'IN_ONESHOT',
+    'IN_ALL_EVENTS',
+]
+
+flag_dict = {
+    name.removeprefix('IN_'): getattr(panzi_inotify, name)
+    for name in flags
+}
+
+SEP = re.compile(r'[+|,\s]')
+
+def _parse_mask(value: str) -> int:
+    mask = 0
+    value = value.strip()
+    if value:
+        for item in SEP.split(value):
+            item = item.strip()
+            uitem = item.upper().replace('-', '_')
+            flag = flag_dict.get(uitem)
+
+            if flag is None:
+                flag = flag_dict.get(uitem.removeprefix('IN_'))
+
+            if flag is None:
+                raise ValueError(f'illegal flag name: {item}')
+
+            mask |= flag
+    return mask
+
+_parse_mask.__name__ = 'event mask'
+
+def main(argv: list[str]):
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--mask',
+        type=_parse_mask,
+        default=IN_ALL_EVENTS,
+        help=f'List of flags.\n'
+             f'Flags: {', '.join(flag.removeprefix('IN_') for flag in flags)}\n'
+             f'[default: ALL_EVENTS]')
+    ap.add_argument('path', nargs='+')
+
+    args = ap.parse_args(argv)
+    mask: int = args.mask
+    paths: list[str] = args.path
 
     with Inotify() as inotify:
-        for filename in sys.argv[1:]:
-            inotify.add_watch(filename)
+        for filename in paths:
+            inotify.add_watch(filename, mask)
 
         for event in inotify:
             print(f'{event.full_path()}: {", ".join(get_inotify_event_names(event.mask))}')
 
 if __name__ == '__main__':
-    main()
+    try:
+        main(sys.argv[1:])
+    except KeyboardInterrupt:
+        print()
