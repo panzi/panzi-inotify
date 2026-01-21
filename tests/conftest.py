ec4ea37c5a7b8e3004027b41225263fda55c34ac
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2026-01-21 01:59:05 +0100

    some very basic tests

---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+
+from typing import Generator
+
+import os
+import sys
+import shutil
+import pytest
+
+from pathlib import Path
+from os.path import join as join_path
+from tempfile import gettempdir
+
+SRC_PATH = str(Path(__file__).resolve().parent.parent)
+
+if SRC_PATH not in sys.path:
+    sys.path.insert(0, SRC_PATH)
+
+@pytest.fixture(scope="function")
+def temp_prefix(request: pytest.FixtureRequest) -> tuple[str, str]:
+    tempdir = gettempdir()
+    PID = os.getpid()
+    func_name = request.function.__name__
+
+    return tempdir, f'logmon.test.{PID}.{func_name}'
+
+@pytest.fixture(scope="function")
+def watch_dir(temp_prefix: tuple[str, str]) -> Generator[str, None, None]:
+    tempdir, prefix = temp_prefix
+    path = join_path(tempdir, prefix)
+    os.mkdir(path)
+    try:
+        yield path
+    finally:
+        try:
+            shutil.rmtree(path)
+        except FileNotFoundError:
+            pass
